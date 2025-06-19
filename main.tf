1672af7712ec4c2fd779463c462ef7795fa46798
Mateo Vega <matiusve09@outlook.com>
2025-06-19 11:30:40 -0500

    proyecto con terraform, localstack y docker desktop

--- main.tf
+++ main.tf
@@ -1,28 +1,8 @@
-variable "instance_type" {
-  type        = string #boolean, string, number, list, map, object
-  description = "Type of instance to use"
-  default     = "c5.large"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-005e54dee72cc1235"
-  instance_type = var.instance_type
+resource "aws_vpc" "main_dr" {
+  provider   = aws.europa
+  cidr_block = "172.16.0.0/16"
 }
-
-output "web_id" {
-  value       = aws_instance.web.arn
-}
-
-data "aws_region" "current" {}
-
-resource "aws_instance" "web1" {
-  ami           = "ami-005e54dee72cc1d00"
-  instance_type = var.instance_type
-}
-
-output region {
-  value       = data.aws_region.current.name
-}
-
-
-
